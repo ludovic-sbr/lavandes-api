0cfc1720b4cbe8f894cd079eeddff00173545137
Ludovic Sbr <sobrero.ludovic@gmail.com>
2022-11-07 16:12:02 +0100

    Initial commit

---
+++ src/test/java/com/feliiks/gardons/feliiks/gardons/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feliiks.gardons.feliiks.gardons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
