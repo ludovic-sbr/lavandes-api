216a9a2b7aeb06757c8c21522f696cb4b81e3e2e
Ludovic Sbr <sobrero.ludovic@gmail.com>
2022-12-26 22:34:48 +0100

    chore : code cleanup

--- src/main/java/com/feliiks/gardons/repositories/FileRepository.java
+++ src/main/java/com/feliiks/gardons/repositories/FileRepository.java
@@ -6,31 +6,16 @@ import com.feliiks.gardons.models.FileModel;
 import com.feliiks.gardons.repositories.jpa.FileJpaRepository;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Optional;
-
 @Component
 public class FileRepository {
     private final FileJpaRepository fileJpaRepository;
     private final FileConverter fileConverter;
 
-    public FileRepository(FileJpaRepository fileJpaRepository, FileConverter fileConverter) {
+    public FileRepository(
+            FileJpaRepository fileJpaRepository,
+            FileConverter fileConverter) {
         this.fileJpaRepository = fileJpaRepository;
         this.fileConverter = fileConverter;
-    }
-
-    public List<FileEntity> findAll() {
-        List<FileModel> allFiles = fileJpaRepository.findAll();
-
-        return allFiles.stream().map(fileConverter::convertToEntity).toList();
-    }
-
-    public Optional<FileEntity> findById(Long id) {
-        Optional<FileModel> file = fileJpaRepository.findById(id);
-
-        if (file.isEmpty()) return Optional.empty();
-
-        return Optional.of(fileConverter.convertToEntity(file.get()));
     }
 
     public FileEntity save(FileEntity file) {
