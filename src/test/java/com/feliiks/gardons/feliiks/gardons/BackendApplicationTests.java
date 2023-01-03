4b356ce31de97180c2016556087b8f5ee85c5a5e
Ludovic Sbr <sobrero.ludovic@gmail.com>
2023-01-03 12:05:05 +0000

    feature : v1.0.0

--- src/test/java/com/feliiks/gardons/feliiks/gardons/BackendApplicationTests.java
+++ src/test/java/com/feliiks/gardons/feliiks/gardons/BackendApplicationTests.java
@@ -1,13 +1,27 @@
 package com.feliiks.gardons.feliiks.gardons;
 
+import com.feliiks.gardons.entities.LocationEntity;
+import com.feliiks.gardons.repositories.LocationRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
 
 @SpringBootTest
 class BackendApplicationTests {
+    @Autowired
+    private LocationRepository repository;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGetAllLocations() {
+        List<LocationEntity> locations = repository.findAll();
+        assertThat(locations).isNotEmpty();
     }
 
 }
